0dda3a2b415a0f77e839ad4d56038fffc0300b65
afrin.farhath <afrinfarhath.mec@gmail.com>
2025-01-06 09:35:25 +0530

    userId updated

--- src/main/java/com/queryapplication/service/QueryService.java
+++ src/main/java/com/queryapplication/service/QueryService.java
@@ -33,7 +33,7 @@ public interface QueryService {
     List<QueryWithAnswersDTO> searchQueries(String questionText, List<String> tags, String tagGroup, String answer);
 
     List<QueryWithAnswersDTO> searchQueriesByKeyword(String keyword);
-    void processFile(MultipartFile file) throws IOException;
+    void processFile(MultipartFile file, Long userId) throws IOException;
 
     void processFileReader(MultipartFile file) throws IOException;
 }
